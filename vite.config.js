cddcdc2e447273e1a32cdfcf347c2e051663a466
Perkiins <erxuulo@gmail.com>
2025-07-03 18:45:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/guide/dep-pre-bundling.html#optimize-deps
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
 });
