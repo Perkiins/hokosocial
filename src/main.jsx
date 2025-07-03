5d521d9121ad105c9e0d3777a4723f7ee2460edd
Perkiins <erxuulo@gmail.com>
2025-07-03 22:02:20 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import './styles/components.css';
+import './styles/panel.css';
+import './styles/register.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
