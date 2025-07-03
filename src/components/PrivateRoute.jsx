f50cb606c7342e375044cbea938b4c71378ce994
Perkiins <erxuulo@gmail.com>
2025-07-03 21:29:36 +0200

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,10 @@
 // src/components/PrivateRoute.jsx
+import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { isAuthenticated } from '../utils/auth';
 
 const PrivateRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
