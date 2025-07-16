b9ee431e4ef8f22405a18459044fdc7e75a9396f
Perkiins <erxuulo@gmail.com>
2025-07-16 19:48:01 +0200

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,40 +1,49 @@
-// src/utils/api.js
 import { getToken } from './auth';
 
-const API_URL = 'https://hokosocial.onrender.com';
+const API = 'https://hokosocial.onrender.com/api';
 
-export const register = async (username, password) => {
-  const res = await fetch(`${API_URL}/register`, {
+export const loginUser = async (username, password) => {
+  const res = await fetch(`${API}/login`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password })
   });
-
   return res.json();
 };
 
-export const login = async (username, password) => {
-  const res = await fetch(`${API_URL}/login`, {
+export const registerUser = async (username, password) => {
+  const res = await fetch(`${API}/register`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password })
   });
-
   return res.json();
 };
 
-export const getProfile = async () => {
-  const token = getToken();
-
-  const res = await fetch(`${API_URL}/profile`, {
+export const getUserData = async () => {
+  const res = await fetch(`${API}/user-data`, {
     headers: {
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${getToken()}`
     }
   });
+  return res.json();
+};
 
+export const runBot = async () => {
+  const res = await fetch(`${API}/run-bot`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${getToken()}`
+    }
+  });
+  return res.json();
+};
+
+export const getLog = async () => {
+  const res = await fetch(`${API}/log`, {
+    headers: {
+      'Authorization': `Bearer ${getToken()}`
+    }
+  });
   return res.json();
 };
